8a5061cfedf71d240ff645a4357bf9519db7b3ac
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2024-03-05 14:48:00 -0300

    Initial Commit

---
+++ PaddingAndSpacerBootcamp/PaddingAndSpacerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddingAndSpacerBootcampApp.swift
+//  PaddingAndSpacerBootcamp
+//
+//  Created by iOS Developer on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PaddingAndSpacerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
