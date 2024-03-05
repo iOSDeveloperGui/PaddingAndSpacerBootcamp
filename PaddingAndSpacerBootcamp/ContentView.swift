45dd73514ab52ace843490c55de5d37affdcea59
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2024-03-05 15:54:50 -0300

    Changing the coner radius value.

--- PaddingAndSpacerBootcamp/ContentView.swift
+++ PaddingAndSpacerBootcamp/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+
+        
+        VStack(alignment: .leading){
             Text("Hello, world!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+                
+                
+            Text("This is the description of what we will do on this screen. It's multiple lines and we will align the text to the leading edge.")
+                .background(Color.white)
+                .foregroundColor(.black)
+                .shadow(radius: 5)
         }
+        
+        .background(Color.blue)
         .padding()
+        .background(Color.red)
+        .padding()
+        .cornerRadius(3)
+        
+                
+        
     }
 }
 
